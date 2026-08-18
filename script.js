94f43a309d5b04b334bca527fdb3925aebbbd87a
SHARSHA-tech <harshashendge@gmail.com>
2026-08-18 20:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name:string,age:number)
+	this.name=name;
+	this.age=age;
+	const person = new Person ("Harsha", 1);
+	console.log(person.name);
+	person.age=25;
+	console.log(person.age);
+}
+get name(string:name) {
+	return name;
+}
+set age(age:number){
+	this.age=age;
+	}
+   console.log(Person);
+class Student extends Person {
 
-class Student extends Person {}
+	const Student = new Student("Elyna",24);
+	student.study();
+	
+	
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	const Teacher = new Teacher("Nisha",22);
+	teacher.teach();
+}
 
 // Do not change the code below this line
 window.Person = Person;
