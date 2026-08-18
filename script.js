1bd490db806fefd6226a0ba784c0f65f45decad6
SHARSHA-tech <harshashendge@gmail.com>
2026-08-18 20:38:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ console.log(age);
 class Student extends Person {
 	study(){
 		console.log(this.name + " is studying");
-	}
+	}}
 class Teacher extends Person {
 teach(){
 	console.log(this.name + " is teaching";)
